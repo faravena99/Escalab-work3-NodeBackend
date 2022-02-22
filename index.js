16fdb22bbbaedc0dd171a1f083d38fd1148be174
Jaime De la Fuente <34488833+Jamesblack1@users.noreply.github.com>
2022-02-21 21:43:57 -0300

    api elements

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
+// Importacion de librerias
 const express = require('express');
 const bodyparser = require('body-parser')
 
 const jsonParser = bodyparser.json();
+const cors = require('cors');
+
 
 
 let usuario = {
@@ -15,7 +18,102 @@ let respuesta = {
     mensaje: ''
 }
 
+let items = [
+    {      
+      name:"Super Mario Kart",
+      description: "Juego de aventuras",
+      platform : "Nintendo",
+      img : "./assets/images/mariokart.jpg"
+    },
+    {
+      name:"Gran turismo",
+      description: "Juego de carreras",
+      platform : "PlayStation",
+      img : "./assets/images/gt.jpg"
+    },
+    {
+      name:"GTA",
+      description: "Juego de aventuras",
+      platform : "Todas las plataformas",
+      img : "./assets/images/gta.jpg"
+    },
+    {
+      name:"Mortal Kombat",
+      description: "Juego de peleas",
+      platform : "Todas las plataformas",
+      img : "./assets/images/mk.jpg"
+    },
+    {
+      name:"Pokemon",
+      description: "Juego de estrategia y aventura",
+      platform : "Nintendo Switch",
+      img : "./assets/images/pokemon.jpg"
+    }
+  ];
+
+
 const app = express();
+
+
+app.use(cors({
+    origin:'*'
+}));
+
+app.get('/products', function(req,res) {
+    console.log("Consultando products")
+    respuesta = {
+        error : false,
+        codigo: 200,
+        mensaje: items
+    };
+    res.send(respuesta);
+})
+
+app.get('/product/:id',jsonParser, function(req,res) {
+    let id = req.params.id        
+    respuesta = {
+        error : false,
+        codigo: 200,
+        mensaje: items[id]
+    };
+    res.send(respuesta);
+})
+
+app.post('/product',jsonParser, function (req, res) {
+    console.log(req.body);
+    if(!req.body){
+        respuesta = {
+            error : false,
+            codigo: 200,
+            mensaje: 'Error creando el nuevo Juego'
+        };
+    }else{       
+        items.push(req.body)        
+        respuesta = {
+            error : false,
+            codigo: 200,
+            mensaje: items
+        };
+        
+    }
+    res.send(respuesta)
+    
+})
+
+app.put('/product/:id', jsonParser, function(req, res){
+    let id = req.params.id    
+    items[id].name = req.body.name
+    items[id].description = req.body.description
+    items[id].platform = req.body.platform
+    items[id].img = req.body.img
+    console.log(items[id])
+    respuesta = {
+        error : false,
+        codigo: 200,
+        mensaje: items[id]
+    };
+    res.send(respuesta);
+})
 
 app.get('/',function(req, res){
     respuesta = {
